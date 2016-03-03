596574e1f703ad3ef52ece8ab8ee40f1b96c0a72
gas131 <rodriguez.cerdeira@gmail.com>
2016-03-03 12:35:42 -0300

    changed class

--- js/src/containers/TopArticle.jsx
+++ js/src/containers/TopArticle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class TopArticle extends React.Component {
 	render() {
 		return(
-				<article className='black'>
+				<article className='row black'>
 					<h1>SIENDO</h1>
 				</article>
 			);
